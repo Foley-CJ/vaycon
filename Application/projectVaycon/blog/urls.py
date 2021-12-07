2996222324b1482267d6b1eba3a437e93cba13c2
Foley-CJ <connorfoley.j@gmail.com>
2021-12-06 22:48:00 -0600

    first push

---
+++ Application/projectVaycon/blog/urls.py
@@ -0,0 +1,18 @@
+
+from django.urls import path, re_path
+from blog import views
+
+urlpatterns = [
+
+    # The home page
+    path('', views.tester, name='home'),
+
+ 	# Upload Location
+    #path('upload/csv', views.upload_csv, name='upload_csv'),
+
+    # Matches any html file
+    #re_path(r'^.*\.*', views.pages, name='pages'),
+
+
+
+]
