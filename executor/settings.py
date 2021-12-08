9f06bdb4371a65f211df0c773467388a14df33e9
Foley-CJ <connorfoley.j@gmail.com>
2021-12-07 21:07:20 -0600

    updated domains

--- executor/settings.py
+++ executor/settings.py
@@ -32,7 +32,7 @@ DEBUG = config('DEBUG',default='False', cast=bool)
 
 # load production server from .env
 #ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['127.0.0.1', config('SERVER',default='127.0.0.1'),'vaycon.com']
+ALLOWED_HOSTS = ['127.0.0.1', config('SERVER',default='127.0.0.1'),'.vaycon.com']
 
 
 
